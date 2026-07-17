45d7399a09b85f3ee3ee82828bf6b77d634bb9d7
alwaysvijayho <chauhanvijay1033@gmail.com>
2026-07-17 17:30:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+API_ID = 36700447
+API_HASH = "093117f52d27c69643b26eb2f16a2015"
+
+FIRST_WARNING = """
+👋 Hello! Har Har Mahadev 
+
+Please wait until I reply.
+
+Sending spam messages may result in an automatic block.
+"""
+
+SECOND_WARNING = """
+⚠️ Warning!
+
+Please don't spam.
+
+If you send one more message before I reply, you will be automatically blocked.
+"""
+
+FINAL_MESSAGE = """
+🚫 You have been automatically blocked.
+
+Reason: Repeated multiple messages before receiving a reply.
+
+You may try contacting me again later.
+"""
+
+# 600 seconds = 10 minutes
+REACTIVATE_AFTER = 600
