86b7504999b0b653140aa821648853ab9ac30da7
Vijay Chauhan <chauhanvijay1033@gmail.com>
2026-07-18 19:07:09 +0530

    Added Meow Meow

--- config.py
+++ config.py
@@ -3,18 +3,22 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
-FIRST_WARNING = """
+FIRST_WARNING = """ 
+Hellow 
 🙏 Har Har Mahadev! 🚩
+jay shree shyam  
 
+ 
 Main is samay vyast hoon.
 
 Kripya apna message ek hi baar bhej dijiye aur mere reply ka dhairya se intezar karein.
 
-Dhanyavaad. 🙏
+" Meow Meow". 🙏
 """
 
 SECOND_WARNING = """
 🙏 Har Har Mahadev! 🚩
+   jay shree shyam 
 
 ⚠️ Warning (2/3)
 
@@ -22,11 +26,12 @@ Aap lagataar messages bhej rahe hain.
 
 Kripya spam na karein.
 
-Agla message bhejne par aap automatically block ho jayenge.
+Agla message bhejne par aap automatically block ho  jaoge " Meow Meow" .
 """
 
 FINAL_WARNING = """
 🙏 Har Har Mahadev! 🚩
+   jay shree shyam 
 
 🚫 Auto Block
 
@@ -34,7 +39,7 @@ Aapne warnings ko ignore kiya.
 
 Spam se suraksha ke liye aapko automatically block kar diya gaya hai.
 
-Dhanyavaad.
+" Meow Meow". 
 """
 
 MAX_MESSAGES = 3
